84874fe93d10b7ea779759c4c0e42768a7cb3337
Sujaany <ssujaany09@gmail.com>
2019-10-03 16:53:27 +0530

    mosh_node_practice ....

--- Mosh_node_practice_E/app.js
+++ Mosh_node_practice_E/app.js
@@ -6,6 +6,57 @@
 
 // console.log(module);
 
-const log = require('./logger');
+// const log = require('./logger');
 
-log('messsage');
+// log('messsage');
+
+// Path Module
+// const path = require('path');
+// var pathObj = path.parse(__filename);
+// console.log(pathObj);
+
+// OS_MODULE
+// const os = require('os');
+// var totalMemory =os.totalmem();
+// var freeMemory =os.freemem();
+// console.log(`Total Memory:' + ${totalMemory}`);
+// console.log(`Free Memory: ${freeMemory}`);
+
+// FS_MODULE
+// const fs = require('fs');
+// fs.readdir('$',function(err,files){
+//     if(err) console.log('Error',err);
+//     else console.log('Result',files);
+// });
+
+// EVENT_MODULE
+// const EventEmitter = require ('events');
+// const emitter = new EventEmitter();
+// Register a listener
+// emitter.on('messageLogged',function(){
+//     console.log('Listener called');
+// });
+// raise an evevnt
+// emitter.emit('messageLogged');
+
+// EVENT_ARGUMENTS
+// const EventEmitter = require ('events');
+// const emitter = new EventEmitter();
+// Register a listener
+// emitter.on('messageLogged',(arg) =>{
+//     console.log('Listener called',arg);
+// });
+// raise an evevnt
+// emitter.emit('messageLogged',{id:1,url:'http://'});
+//Raise: logging (data:message)
+
+// EXTENDING_EVENT_EMMITER
+// const EventEmitter = require ('events');
+// const emitter =new EventEmitter();
+
+// emitter.on('messageLogged',(arg) =>{
+//     console.log('Listener called',arg);
+// });
+
+// logger.log('message');
+
--- Mosh_node_practice_E/logger.js
+++ Mosh_node_practice_E/logger.js
@@ -1,8 +1,31 @@
-var url='http://mylogger.io/log';
+// console.log(__filename);
+// console.log(__dirname);
 
-function log (message){
-    console.log(message);
-}
+// var url='http://mylogger.io/log';
 
-module.exports=log;
+// function log (message){
+//     console.log(message);
+// }
+
+// module.exports=log;
+
+// EXTENDING_EVENT_EMMITER
+// const EventEmitter = require ('events');
+// const emitter =new EventEmitter();
+
+// var url ='http://mylogger.io/log';
+
+// class Logger extends EventEmitter {
+//     log(message){
+        //send an HTTP request
+        // console.log(message);
+
+        //Raise an event
+//         this.emit('messageLogged',{id:1, url:'http://'});
+//     }
+// }
+// module.exports =Logger;
+
+
+
 
